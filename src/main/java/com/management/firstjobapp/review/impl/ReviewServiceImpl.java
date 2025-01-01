a4028ed33be2417ae05160b6d8a512c263e6d248
Eligert Uzuni <eligertuzuni@gmail.com>
2025-01-01 16:28:40 +0100

    getAllReviews on ReviewServiceImpl

--- src/main/java/com/management/firstjobapp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/management/firstjobapp/review/impl/ReviewServiceImpl.java
@@ -1,9 +1,23 @@
 package com.management.firstjobapp.review.impl;
 
+import com.management.firstjobapp.review.Review;
+import com.management.firstjobapp.review.ReviewRepository;
 import com.management.firstjobapp.review.ReviewService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReviewServiceImpl implements ReviewService {
+    private ReviewRepository reviewRepository;
 
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public List<Review> getAllReviews(Long companyId) {
+        List<Review> reviews =
+        return List.of();
+    }
 }
